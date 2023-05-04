elemental-lf/NetworkManager-openconnect

commit 3f7afad9d3ca8a69fed10b688ad75c45281447e9

Clean up initial host URL parsing
We don't need to mess around with the authgroup handling because that
has been handled for us by openconnect_parse_url() for over a decade...

Fixes: #71

auth-dialog/main.c
@@ -1212,16 +1212,10 @@ static int get_config (auth_ui_data *ui_data,
 	}
 
 	/* add gateway to host list */
-	vpnhosts = malloc(sizeof(*vpnhosts));
+	vpnhosts = g_malloc0(sizeof(*vpnhosts));
 	if (!vpnhosts)
 		return -ENOMEM;
 	vpnhosts->hostname = g_strdup(hostname);
-	group = strchr(vpnhosts->hostname, '/');
-	if (group) {
-		*(group++) = 0;
-		vpnhosts->usergroup = g_strdup(group);
-	} else
-		vpnhosts->usergroup = NULL;
 	vpnhosts->hostaddress = g_strdup (hostname);
 	vpnhosts->next = NULL;
 

commit 863638987c5e2e9630f1a88c04cb50f4fb1c7401

Remove unused group variable
Fixes: 3f7afad9d3ca ("Clean up initial host URL parsing")

auth-dialog/main.c
@@ -1188,7 +1188,6 @@ static int get_config (auth_ui_data *ui_data,
 	char *proxy;
 	char *xmlconfig;
 	char *hostname;
-	char *group;
 	char *csd;
 #if OPENCONNECT_CHECK_VER(5,8)
 	char *mcakey, *mcacert, *mca_key_pass;