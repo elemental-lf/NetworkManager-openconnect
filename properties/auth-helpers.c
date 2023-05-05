elemental-lf/NetworkManager-openconnect

commit 92e93976264e69cee708a13d3f4f7006d7a2593b

properties: load certificates & key from the connection
This has been omitted whilst porting to the Gtk4-ready NmaCertChooser.

[dwmw2: Update for MCA cert too, provide scheme to fix filenames]
Fixes: 650bfa6f60f8 ('properties: use NmaCertChooser')

properties/auth-helpers.c
@@ -43,6 +43,7 @@ tls_pw_init_auth_widget (GtkBuilder *builder,
                          gpointer user_data)
 {
 	GtkWidget *widget;
+	const char *value;
 	GtkSizeGroup *group;
 
 	g_return_if_fail (builder != NULL);
@@ -54,13 +55,53 @@ tls_pw_init_auth_widget (GtkBuilder *builder,
 	nma_cert_chooser_add_to_size_group (NMA_CERT_CHOOSER (widget), group);
 	g_signal_connect (G_OBJECT (widget), "changed", G_CALLBACK (changed_cb), user_data);
 
+	if (s_vpn) {
+		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CACERT);
+		if (value && strlen (value))
+			nma_cert_chooser_set_cert_uri (NMA_CERT_CHOOSER (widget), value);
+	}
+
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "mca_cert_chooser"));
 	nma_cert_chooser_add_to_size_group (NMA_CERT_CHOOSER (widget), group);
 	g_signal_connect (G_OBJECT (widget), "changed", G_CALLBACK (changed_cb), user_data);
 
+	if (s_vpn) {
+		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_MCACERT);
+		if (value && strlen (value))
+			nma_cert_chooser_set_cert (NMA_CERT_CHOOSER (widget), value,
+									   g_str_has_prefix(value, "pkcs11:") ?
+									   NM_SETTING_802_1X_CK_SCHEME_PKCS11 :
+									   NM_SETTING_802_1X_CK_SCHEME_PATH);
+
+		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_MCAKEY);
+		if (value && strlen (value))
+			nma_cert_chooser_set_key (NMA_CERT_CHOOSER (widget), value,
+									  g_str_has_prefix(value, "pkcs11:") ?
+									  NM_SETTING_802_1X_CK_SCHEME_PKCS11 :
+									  NM_SETTING_802_1X_CK_SCHEME_PATH);
+	}
+
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "user_cert_chooser"));
 	nma_cert_chooser_add_to_size_group (NMA_CERT_CHOOSER (widget), group);
 	g_signal_connect (G_OBJECT (widget), "changed", G_CALLBACK (changed_cb), user_data);
+
+	if (s_vpn) {
+		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_USERCERT);
+		if (value && strlen (value))
+			nma_cert_chooser_set_cert (NMA_CERT_CHOOSER (widget), value,
+									   g_str_has_prefix(value, "pkcs11:") ?
+									   NM_SETTING_802_1X_CK_SCHEME_PKCS11 :
+									   NM_SETTING_802_1X_CK_SCHEME_PATH);
+
+		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_PRIVKEY);
+		if (value && strlen (value)) {
+			nma_cert_chooser_set_key (NMA_CERT_CHOOSER (widget), value,
+									  g_str_has_prefix(value, "pkcs11:") ?
+									  NM_SETTING_802_1X_CK_SCHEME_PKCS11 :
+									  NM_SETTING_802_1X_CK_SCHEME_PATH);
+
+		}
+	}
 }
 
 gboolean

commit 85f05b3f962966ae06d4d388770bb5aa65bb0b7e

Fix loading of CA cert field from config
I fixed up the client cert/key not to use the _uri() form of the functions,
and to explicitly specify PKCS11/PATH scheme. Missed the CA cert field.

Fixes: 92e93976264e ("properties: load certificates & key from the connection")

properties/auth-helpers.c
@@ -58,7 +58,8 @@ tls_pw_init_auth_widget (GtkBuilder *builder,
 	if (s_vpn) {
 		value = nm_setting_vpn_get_data_item (s_vpn, NM_OPENCONNECT_KEY_CACERT);
 		if (value && strlen (value))
-			nma_cert_chooser_set_cert_uri (NMA_CERT_CHOOSER (widget), value);
+			nma_cert_chooser_set_cert (NMA_CERT_CHOOSER (widget), value,
+									   NM_SETTING_802_1X_CK_SCHEME_PATH);
 	}
 
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "mca_cert_chooser"));